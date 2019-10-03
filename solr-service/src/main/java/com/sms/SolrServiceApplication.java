0e1d451ab34b02a389f426f06bb7146a6c52fcaf
mathuranchal73 <Anchal.Mathur@BOLD-ANCHALM>
2019-10-03 16:33:28 +0530

    03Oct5PM

---
+++ solr-service/src/main/java/com/sms/SolrServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolrServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolrServiceApplication.class, args);
+	}
+
+}
